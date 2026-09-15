563b24f2ec49d6799c20e0f9a23b8a9e11d97994
yura <karastop123@gmail.com>
2026-09-15 16:38:21 +0300

    фотодатчик

---
+++ get-led/auto-light.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time 
+GPIO.setmode(GPIO.BCM)
+
+
+led  =26
+GPIO.setup(led, GPIO.OUT)
+
+photo = 6
+GPIO.setup(photo, GPIO.IN)
+state = 0
+
+while True:
+    if GPIO.input(photo) == 0:
+        state = 1
+        GPIO.output(led, state)
+    else:
+        state = 0
+        GPIO.output(led, state)
+
+
